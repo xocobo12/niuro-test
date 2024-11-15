28d1a996c89000021e455bd73f8015532596a5d3
Rene <rsilvav@Renes-MacBook-Air.local>
2024-11-15 17:55:50 -0300

    chore: flake8 for test_auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,8 @@
 import pytest
 from src.auth import AuthManager
-from src.hash_utils import hash_password
 from dotenv import load_dotenv
 import os
+
 
 @pytest.fixture(scope="module")
 def auth_manager():
@@ -10,6 +10,7 @@ def auth_manager():
     Fixture to create an instance of AuthManager.
     """
     return AuthManager()
+
 
 @pytest.fixture(scope="module")
 def credentials():
@@ -20,8 +21,9 @@ def credentials():
     username = os.getenv("USER_1")
     password = os.getenv("PASSWORD_1")
     if not username or not password:
-        raise ValueError("Username or password is not set in the environment variables.")
+        raise ValueError("Username or password is not set in the env vars.")
     return username, password
+
 
 def test_authenticate_user(auth_manager, credentials):
     """
@@ -30,7 +32,9 @@ def test_authenticate_user(auth_manager, credentials):
     username, password = credentials
     # Authenticate user
     token = auth_manager.authenticate_user(username, password)
-    assert token is not False, "Authentication failed with correct credentials."
+    auth_success = token is not False
+    assert auth_success, "Authentication failed with correct credentials"
+
 
 def test_authenticate_user_wrong_password(auth_manager, credentials):
     """
@@ -40,4 +44,5 @@ def test_authenticate_user_wrong_password(auth_manager, credentials):
 
     # Attempt to authenticate with an incorrect password
     token = auth_manager.authenticate_user(username, "wrong_password")
-    assert token is False, "Authentication should fail with an incorrect password."
+    auth_fail = token is False
+    assert auth_fail, "Authentication should fail with an incorrect password."
