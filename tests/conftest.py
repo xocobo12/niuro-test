ba884968c3e6c6dd030d78487cf8fc560d0731c6
Rene <rsilvav@Renes-MacBook-Air.local>
2024-11-18 18:23:19 -0300

    feat: add credentials to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
-from your_project.auth import AuthManager
+import os
+from src.auth import AuthManager
+from dotenv import load_dotenv
+
 
 @pytest.fixture(scope="module")
 def auth_manager():
@@ -7,3 +10,16 @@ def auth_manager():
     Fixture to create an instance of AuthManager.
     """
     return AuthManager()
+
+
+@pytest.fixture(scope="module")
+def credentials():
+    """
+    Fixture to load credentials from environment variables.
+    """
+    load_dotenv()
+    username = os.getenv("USER_1")
+    password = os.getenv("PASSWORD_1")
+    if not username or not password:
+        raise ValueError("Username or password is not set in the env vars.")
+    return username, password
