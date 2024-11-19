1fd2adbe7d713662d9603abe394e15f843ef6596
Rene <rsilvav@Renes-MacBook-Air.local>
2024-11-19 09:50:51 -0300

    rebase: deleted credentials from test_auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,20 +1,6 @@
 import pytest
-from src.auth import AuthManager
 from dotenv import load_dotenv
 import os
-
-
-@pytest.fixture(scope="module")
-def credentials():
-    """
-    Fixture to load credentials from environment variables.
-    """
-    load_dotenv()
-    username = os.getenv("USER_1")
-    password = os.getenv("PASSWORD_1")
-    if not username or not password:
-        raise ValueError("Username or password is not set in the env vars.")
-    return username, password
 
 
 def test_authenticate_user(auth_manager, credentials):
