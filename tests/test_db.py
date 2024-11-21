6dd7e02fd3a88377d6ac178b06b2515fb4ba25b0
Rene <rsilvav@Renes-MacBook-Air.local>
2024-11-21 11:30:23 -0300

    chore: pep8 for test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from src.db import DBClient
 
 
 def test_users_table(db_client):
-    #import streamlit as st
     # Query and display the data you inserted
     users = db_client.conn.query('select * from users')
     st_df = st.dataframe(users)
